77fc89be17eb11b11b9a02e81fe3d156642c5f3f
Roman Bylbas <romka.bulbas@gmail.com>
2015-12-10 17:47:49 -0800

    Added auth validation

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+$('document').ready(function(){
+	/*
+	 * Hiden elements
+	*/
+	var unauthorizedElems = document.getElementsByClassName('unauthorized');
+	var authorizedElems = document.getElementsByClassName('authorized');
+
+	if(window.localStorage.getItem('token').length === 16){
+		for(key in unauthorizedElems){
+			if(unauthorizedElems[key] instanceof HTMLElement){
+				unauthorizedElems[key].style.display = 'none';
+			}
+		}
+	} else {
+		for(key in authorizedElems){
+			if(authorizedElems[key] instanceof HTMLElement){
+				authorizedElems[key].style.display = 'none';
+			}
+		}
+	}
+
+	/*
+	 * Logout
+	*/
+	$('#logout').on('click', function(e){
+		console.log('click');
+		window.localStorage.setItem('token', '');
+	});
+});
